a8f408d99828347fe3efb0eed822ca7811c75c78
GabrielNicolasAvellaneda <avellaneda.gabriel@gmail.com>
2015-08-05 19:45:26 -0700

    Added test for riak class with repositories disabled for Ubuntu.
    Signed-off-by: GabrielNicolasAvellaneda <avellaneda.gabriel@gmail.com>

--- spec/classes/riak_spec.rb
+++ spec/classes/riak_spec.rb
@@ -55,7 +55,12 @@ describe 'riak' do
             it { is_expected.to_not contain_class('riak::repository::el') }
             it { is_expected.to_not contain_yumrepo('basho_riak') }
           when 'Debian'
-            it { is_expected.to_not contain_class('riak::repository::debian') }
+            case facts[:operationsystem]
+            when 'ubuntu'
+              it { is_expected.to_not contain_class('riak::repository::ubuntu') }
+            else
+              it { is_expected.to_not contain_class('riak::repository::debian') }  
+            end
             it { is_expected.to_not contain_apt__source('riak') }
           end
         end
